e790d2eb597117a131fb1299afdb6926eee01ddb
CoCoL0r3s <grillmayer@mailbox.org>
2021-01-06 20:58:08 +0100

    updated ignore and imported data

--- expA.py
+++ expA.py
@@ -1,6 +1,10 @@
+# load csv modul
 import csv
 
+# import csv into list of dicts
+import_list = []
 with open("python-portfolio-project-starter-files/insurance.csv") as insurance_file:
     fields = ["age", "sex", "bmi", "children", "smoker", "region", "charges"]
     data_reader = csv.DictReader(insurance_file, fields)
-    print(list(data_reader))
+    for row in data_reader:
+        import_list.append(row)
